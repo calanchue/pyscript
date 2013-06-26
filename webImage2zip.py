799be8640be5c4bbaafc949d5323f88b0f620c0e
gm1302463 <gm1302463@gm1302463-PC.gabiasafe.com>
2013-06-26 15:23:34 +0900

    complete : using crawler class, downlaod img files and zip it to save some where.

--- webImage2zip.py
+++ webImage2zip.py
@@ -13,32 +13,132 @@ from urllib2 import urlopen
 from urllib import urlretrieve
 import os
 import sys
+from zipfile import ZipFile, ZipInfo, ZIP_DEFLATED
+import zlib
+from cStringIO import StringIO
+from urllib2 import urlopen
+from os import path
+import string
+
+
+class SiteCrawler():
+    valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
+    
+    def __init__(self, first_page_url):
+        self.page_count = 1
+        self.first_page_url = first_page_url
+        self.curr_page_soup = bs(urlopen(first_page_url))
+        self.curr_page_url = first_page_url
+        self.parsed = list(urlparse.urlparse(first_page_url))
+    
+    """must override """
+    def _getImageSourceUrl(self):
+        pass
+    
+    """must override """
+    def _getNextPageUrl(self):
+        pass
+    
+    """optional override """
+    def getTitle(self):
+        return ''.join(c for c in self.parsed[2] if c in SiteCrawler.valid_chars)
+        
+    """return a next page url string. if there are not, return None"""
+    def jumpToNextPage(self):
+        url = self._getNextPageUrl()
+        if not url:
+            return None
+        self.curr_page_soup = bs(urlopen(url))
+        self.curr_page_url = url
+        self.page_count += 1
+        return self    
+    
+    def getCurrentPageNumber(self):
+        return self.page_count
+    
+    def getFullImageUrl(self):
+        raw_src = self._getImageSourceUrl().lower()
+        if raw_src.startswith("http") or raw_src.startswith("https"):
+            return raw_src
+        else:
+            self.parsed[2] = raw_src
+            return urlparse.urlunparse(self.parsed)
+    def getAllImageUrl(self):
+        while(True):
+            yield self.getFullImageUrl()
+            if not self.jumpToNextPage():
+                break
+
+    
+class CwNoah(SiteCrawler):
+    def _getImageSourceUrl(self):
+        pass
+    
+    def _getNextPageUrl(self):
+        pass
+
+class CwH2R(SiteCrawler):
+    def _getImageSourceUrl(self):
+        pass
+        
+    def _getNextPageUrl(self):
+        pass
+class CwEH(SiteCrawler):
+    def _getImageSourceUrl(self):
+        pass
+        
+    def _getNextPageUrl(self):
+        pass
+
+class CwGM(SiteCrawler):
+    def _getImageSourceUrl(self):
+        pass
+    
+    def _getNextPageUrl(self):
+        pass
+
+class CwDummy(SiteCrawler):
+    def __init__(self):
+        SiteCrawler.__init__(self, "http://noah.kaist.ac.kr/Circle/HAJE/seminar/1d1p")
+        self.dummyUrl = ["/static/thumbs_desktop/547160/12_0.png", 
+                         "/static/thumbs_desktop/547160/12_1.png",
+                         "/static/thumbs_desktop/547160/12_2.png"
+                         ]
+    
+    def _getImageSourceUrl(self):
+        if self.page_count-1 < self.dummyUrl.__len__():
+            return self.dummyUrl[self.page_count-1]
+        
+    
+    def _getNextPageUrl(self):
+        if self.page_count  < self.dummyUrl.__len__():
+            return self.first_page_url
+
+
+def getImageFormSrc(src):
+    pass
+
+def crawlImg2Zip(crawler, target_dir):
+    buf = StringIO()
+    zip_file = ZipFile(buf, mode='w')
+    
+    for img_url in crawler.getAllImageUrl():
+        image = urlopen(img_url)
+        print "imgUrl : ", img_url
+        ext = img_url.split(".")[-1]
+        fname= path.basename("%03d.%s" % (crawler.getCurrentPageNumber(),ext))
+        zip_file.writestr(fname, image.read())
+    zip_file.close()
+    
+    zip_file_name = "%s.zip" % crawler.getTitle()
+    output = open(os.path.join(target_dir, zip_file_name), 'wb')
+    output.write(buf.getvalue())
+    output.close()
+    buf.close()
 
 def main(url, out_folder="/test/"):
-    """Downloads all the images at 'url' to /test/"""
-    soup = bs(urlopen(url))
-    parsed = list(urlparse.urlparse(url))
-    
-    for image in soup.findAll("img"):
-        print "Image: %(src)s" % image
-        filename = image["src"].split("/")[-1]
-        parsed[2] = image["src"]
-        outpath = os.path.join(out_folder, filename)
-        if image["src"].lower().startswith("http"):
-            urlretrieve(image["src"], outpath)
-        else:
-            urlretrieve(urlparse.urlunparse(parsed), outpath)
-
-def _usage():
-    print "usage: python dumpimages.py http://example.com [outpath]"
+    os.path.isdir(out_folder)
+    crawlImg2Zip(CwDummy(), out_folder)  
 
 if __name__ == "__main__":
-    url = sys.argv[-1]
-    out_folder = "/test/"
-    if not url.lower().startswith("http"):
-        out_folder = sys.argv[-1]
-        url = sys.argv[-2]
-        if not url.lower().startswith("http"):
-            _usage()
-            sys.exit(-1)
-    main(url, out_folder)
+    main("asdfasdf", "d://var/")
